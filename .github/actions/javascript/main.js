decd68fdfdc1efb92ba33c690f22b8c0cd645ec1
Ichi-Miike <mat.smith@epssw.com>
2024-04-05 11:10:28 +0100

    Added inputs to Javascript action

--- .github/actions/javascript/main.js
+++ .github/actions/javascript/main.js
@@ -4,9 +4,25 @@ const exec = require('@actions/exec');
 
 function run() {
  
-    core.notice('This is a message printed from core.notice');
+    //  1. Get inputs
+    const bucket = core.getInput('bicket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder' {required: true});
 
-    console.log('This one comes from console.log');
+    //  2. Upload files
+    const s3Uri = `s3://${bucket}`
+    //exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+
+    core.notice('This is a message printed from core.notice');
+    console.log();
+    
+    core.notice(`Bucket: ${bucket}`);
+    core.notice(`Bucket Region: ${bucketRegion}`);
+    core.notice(`Dist Folder: ${distFolder}`);
+    
+
+
 }
 
 run();
